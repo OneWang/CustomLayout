b7bc494f629cf292cb9ad63bf49feeefa22248b8
wangqingfei <wangqingfei@jiayuan.com>
2018-08-15 15:35:10 +0800

    optimize project

--- CustomLayout/LineLayout.m
+++ CustomLayout/LineLayout.m
@@ -13,8 +13,7 @@ static const CGFloat ItemHW = 150;
 @implementation LineLayout
 
 /** 一些初始化的工作最好在这里实现 */
-- (void)prepareLayout
-{
+- (void)prepareLayout{
     [super prepareLayout];
     
     //初始化每个 cell 的尺寸
@@ -23,23 +22,21 @@ static const CGFloat ItemHW = 150;
     self.sectionInset = UIEdgeInsetsMake(0, inset, 0, inset);
     //设置水平滚动
     self.scrollDirection = UICollectionViewScrollDirectionHorizontal;
-    self.minimumLineSpacing = ItemHW * 0.3;
+    self.minimumLineSpacing = ItemHW * 0.1;
     //每一个 Item 都有自己的UICollectionViewLayoutAttributes
     //每一个 indexpath 都有自己的UICollectionViewLayoutAttributes
 }
 
 /** 只要显示的边界发生改变就重新布局,内部重新调用layoutAttributesForElementsInRect方法获得 cell 布局的属性 */
-- (BOOL)shouldInvalidateLayoutForBoundsChange:(CGRect)newBounds
-{
+- (BOOL)shouldInvalidateLayoutForBoundsChange:(CGRect)newBounds{
     return YES;
 }
 
-/** 有效距离:当item的中间x距离屏幕的中间x在HMActiveDistance以内,才会开始放大, 其它情况都是缩小 */
-static CGFloat const HMActiveDistance = 150;
+/** 有效距离:当item的中间x距离屏幕的中间x在ActiveDistance以内,才会开始放大, 其它情况都是缩小 */
+static CGFloat const ActiveDistance = 150;
 /** 缩放因素: 值越大, item就会越大 */
-static CGFloat const HMScaleFactor = 0.4;
-- (NSArray<UICollectionViewLayoutAttributes *> *)layoutAttributesForElementsInRect:(CGRect)rect
-{
+static CGFloat const ScaleFactor = 0.5;
+- (NSArray<UICollectionViewLayoutAttributes *> *)layoutAttributesForElementsInRect:(CGRect)rect{
     // 0.计算可见的矩形框
     CGRect visiableRect;
     visiableRect.size = self.collectionView.frame.size;
@@ -61,8 +58,9 @@ static CGFloat const HMScaleFactor = 0.4;
         
         // 差距越小, 缩放比例越大
         // 根据跟屏幕最中间的距离计算缩放比例
-        CGFloat scale = 1 + HMScaleFactor * (1 - (ABS(itemCenterX - centerX) / HMActiveDistance));
+        CGFloat scale = 1 + ScaleFactor * (1 - (ABS(itemCenterX - centerX) / ActiveDistance));
         attrs.transform = CGAffineTransformMakeScale(scale, scale);
+//        attrs.transform3D = CATransform3DMakeScale(scale, scale, -1);
     }
     return array;
 }
@@ -72,8 +70,7 @@ static CGFloat const HMScaleFactor = 0.4;
  *  proposedContentOffset:原本scrollview 停止滚动哪一刻的位置
  *  velocity:滚动速度
  */
-- (CGPoint)targetContentOffsetForProposedContentOffset:(CGPoint)proposedContentOffset withScrollingVelocity:(CGPoint)velocity
-{
+- (CGPoint)targetContentOffsetForProposedContentOffset:(CGPoint)proposedContentOffset withScrollingVelocity:(CGPoint)velocity{
     // 1.计算出scrollView最后会停留的范围
     CGRect lastRect;
     lastRect.origin = proposedContentOffset;
@@ -93,7 +90,6 @@ static CGFloat const HMScaleFactor = 0.4;
             adjustOffsetX = attrs.center.x - centerX;
         }
     }
-    
     return CGPointMake(proposedContentOffset.x + adjustOffsetX, proposedContentOffset.y);
 }
 
--- CustomLayout/ViewController.m
+++ CustomLayout/ViewController.m
@@ -23,24 +23,15 @@
 @end
 
 @implementation ViewController
+
 static NSString *const ID = @"image";
-- (NSMutableArray *)images
-{
-    if (!_images) {
-        _images = [NSMutableArray array];
-        for (int i = 1; i < 20; i++) {
-            [self.images addObject:[NSString stringWithFormat:@"%d",i]];
-        }
-    }
-    return _images;
-}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    CGRect rect = CGRectMake(0, 100, k_Screen_width,300);
+    CGRect rect = CGRectMake(0, 50, k_Screen_width,300);
     UICollectionView *collectionView = [[UICollectionView alloc] initWithFrame:rect collectionViewLayout:[[CircleLayout alloc] init]];
-    collectionView.backgroundColor = [UIColor lightGrayColor];
+    collectionView.backgroundColor = [UIColor whiteColor];
     collectionView.delegate = self;
     collectionView.dataSource = self;
     collectionView.showsHorizontalScrollIndicator = NO;
@@ -49,34 +40,44 @@ static NSString *const ID = @"image";
     self.collectionView = collectionView;
 }
 
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
-{
-    if ([self.collectionView.collectionViewLayout isKindOfClass:[StackLayout class]]) {
-        [self.collectionView setCollectionViewLayout:[[CircleLayout alloc] init] animated:YES];
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    if ([_collectionView.collectionViewLayout isKindOfClass:[StackLayout class]]) {
+        [_collectionView setCollectionViewLayout:[[LineLayout alloc] init] animated:YES];
+    }else if ([_collectionView.collectionViewLayout isKindOfClass:[LineLayout class]]){
+        [_collectionView setCollectionViewLayout:[[CircleLayout alloc] init] animated:YES];
     }else{
-        [self.collectionView setCollectionViewLayout:[[StackLayout alloc] init] animated:YES];
+        [_collectionView setCollectionViewLayout:[[StackLayout alloc] init] animated:YES];
     }
 }
 
 #pragma mark UICollectionViewDataSource
-- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
-{
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
     return self.images.count;
 }
-- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
-{
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
     ImageCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:ID forIndexPath:indexPath];
     cell.imageName = self.images[indexPath.item];
     return cell;
 }
 
 #pragma mark UICollectionViewDelegate
-//- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
-//{
+//- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath{
 //    //删除模型数据
 //    [self.images removeObjectAtIndex:indexPath.item];
 //    //删 UI(刷新 UI 界面)
 //    [collectionView deleteItemsAtIndexPaths:@[indexPath]];
 //}
+
+#pragma mark - setter and getter
+- (NSMutableArray *)images{
+    if (!_images) {
+        _images = [NSMutableArray array];
+        for (int i = 1; i < 20; i++) {
+            [self.images addObject:[NSString stringWithFormat:@"%d",i]];
+        }
+    }
+    return _images;
+}
 
 @end
